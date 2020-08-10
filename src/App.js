6c15903aca4ae4b80914a045a3ce69cdbe5ddf3d
deji adesoga <done2stay@gmail.com>
2020-08-10 01:17:51 +0100

    Added Basic Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import "app.css";
+import http from "./http.jpg";
+import Nav from "./nav";
 
 const App = () => (
   <div>
-    <h1>Hello React</h1>
+    <Nav />
+    <h1 className="bgColor">Hello React!!!</h1>
+    <img src={http} />
   </div>
 )
 
---
+++ src/Nav.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Nav = () => (
+  <nav>
+    <h1>Logo</h1>
+
+    <ul>
+      <ol>
+        Home
+      </ol>
+      <ol>
+        About
+      </ol>
+      <ol>
+        Service
+      </ol>
+      <ol>
+        Contact
+      </ol>
+    </ul>
+  </nav>
+)
+
+export default Nav
